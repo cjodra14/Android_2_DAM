9eeaebbce3680bd1a8766c9059c7f778ba87b5a5
Christian Jodra <c.jodra14@gmail.com>
2021-10-13 17:42:24 +0200

    Ejemplo de diferentes templates
    Portrait y landascape

---
+++ TestAndroid/app/src/main/java/com/example/testandroid/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.testandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
