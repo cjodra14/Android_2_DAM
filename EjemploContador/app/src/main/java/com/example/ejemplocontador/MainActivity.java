2dac9c2be4012813469959e0be1071e2d9120ac8
Christian Jodra <c.jodra14@gmail.com>
2021-10-20 01:47:39 +0200

    Intents, Recursos y creacion proyecto geolocalizacion

--- EjemploContador/app/src/main/java/com/example/ejemplocontador/MainActivity.java
+++ EjemploContador/app/src/main/java/com/example/ejemplocontador/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.ejemplocontador;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
@@ -10,8 +13,6 @@ public class MainActivity extends Activity {
 
     public int contadorPulsaciones;
     public final int ZERO= 0;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,25 +20,42 @@ public class MainActivity extends Activity {
         contadorPulsaciones=ZERO;
         visualizarContador();
     }
-
-
-
    public void m_incrementar(View vista1){
         contadorPulsaciones++;
        visualizarContador();
     }
-
     public void m_decrementar(View vista1){
+        CheckBox cb_negativo = (CheckBox) findViewById(R.id.checkNegativos);
+        if(cb_negativo.isChecked() && contadorPulsaciones<1){
         contadorPulsaciones--;
         visualizarContador();
+        }else if (cb_negativo.isChecked()&&contadorPulsaciones>0){
+            contadorPulsaciones--;
+            visualizarContador();
+        }if (!cb_negativo.isChecked()&& contadorPulsaciones>0){
+            contadorPulsaciones--;
+            visualizarContador();
+        }
     }
 
     public void m_resetearContador(View vista1){
-        contadorPulsaciones=ZERO;
+        EditText editText = (EditText) findViewById(R.id.editTextReset);
+        try {
+            contadorPulsaciones= Integer.parseInt(editText.getText().toString());
+            ocultarTeclado(editText);
+        }catch (Exception e){
+            contadorPulsaciones= 0;
+            ocultarTeclado(editText);
+        }
+
         visualizarContador();
     }
     public void visualizarContador(){
         TextView contador =findViewById(R.id.tv_contador_numeros);
-        contador.setText(Integer.toString(contadorPulsaciones));
+        contador.setText(String.valueOf(contadorPulsaciones));
+    }
+    public void ocultarTeclado(EditText ed){
+        InputMethodManager miteclado = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        miteclado.hideSoftInputFromWindow(ed.getWindowToken(),0);
     }
 }
--- PrimeraApp/app/src/main/java/com/example/primeraapp/MainActivity.java
+++ PrimeraApp/app/src/main/java/com/example/primeraapp/MainActivity.java
@@ -12,12 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Button boton1 = findViewById(R.id.boton1);
         Button boton2 = findViewById(R.id.boton2);
         Button botonSalir = findViewById(R.id.botonSalir);
-            boton1.setOnClickListener(view -> Toast.makeText(getApplicationContext(), "este es el boton 1", Toast.LENGTH_LONG).show());
-        boton2.setOnClickListener(view ->
-                Toast.makeText(getApplicationContext(), "este es el boton 2", Toast.LENGTH_LONG).show());
+
+        boton1.setOnClickListener(view -> Toast.makeText(getApplicationContext(), "este es el boton 1", Toast.LENGTH_LONG).show());
+        boton2.setOnClickListener(view -> Toast.makeText(getApplicationContext(), "este es el boton 2", Toast.LENGTH_LONG).show());
 
         botonSalir.setOnClickListener( view-> this.finishAndRemoveTask());
     }
